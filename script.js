fecd980e13648f347205b45f24f917cd7116f582
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-10-04 13:23:48 +0000

    parseInt used for adding two Numbers

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function add() {
     const inputElement1 = document.getElementById('num1');
     const inputElement2 = document.getElementById('num2');
+
+    const sum = parseInt(inputElement1.value) + parseInt(inputElement2.value);
+
     
 } 
