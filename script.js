e0abbb45d5f15493ca993369d425d982f8054e11
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-10-05 14:32:17 +0000

    -

--- script.js
+++ script.js
@@ -16,8 +16,10 @@ function add() {
     const inputElement1 = document.getElementById('num1');
     const inputElement2 = document.getElementById('num2');
 
-    const sum = parseInt(inputElement1.value) - parseInt(inputElement2.value);
+    const sub = parseInt(inputElement1.value) - parseInt(inputElement2.value);
 
+    const resultElement = document.getElementById('result');
+    resultElement.innerText = `Result :${sub}`;
    }
 
 // Function for Clearing the Input Boxes
